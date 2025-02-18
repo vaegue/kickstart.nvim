7cd2ff0dae12ba8d9a4a7ee2d1a7bf3d1bbe7be8
sgrimes3 <sgrimes1@pacific.edu>
2025-02-18 02:48:06 -0800

    rearrange custom plugins

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -11,5 +11,44 @@ return {
       }
     end,
   },
+  {
+    'olimorris/onedarkpro.nvim',
+    priority = 1000,
+    config = function()
+      require('onedarkpro').setup {
+        highlights = {},
+        options = {
+          transparency = true,
+          -- highlight_inactive_windows = true,
+          cursorline = true,
+        },
+      }
+    end,
+  },
   { 'UtkarshVerma/molokai.nvim', priority = 1000 },
+  -- {
+  --   'adamkali/vaporlush',
+  --   branch = 'v2', -- must be set until stable
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     cache = true, -- cache the currently held
+  --     -- style = 'vapor',
+  --     -- style = 'blossom',
+  --     style = '1996',
+  --   },
+  -- },
+  -- {
+  --   'adamkali/vaporlush',
+  --   priority = 1000,
+  --   name = 'vaporlush',
+  --   dependencies = {
+  --     'rktjmp/lush.nvim',
+  --   },
+  --   require('vaporlush').setup {
+  --     options = {
+  --       transparency = true,
+  --     },
+  --   },
+  -- },
 }
